a6499b0984262974361d17b2c0530b69c39f3fff
Charles Kennedy <82charkenn@csdecatur.net>
2025-06-13 22:41:40 -0400

    added a ton of empty files, main just for structuring

---
+++ FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/elevator/Elevator.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.superstructure.elevator;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+public class Elevator {
+    
+}
---
+++ FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/wrist/Wrist.java
@@ -0,0 +1,5 @@
+package frc.robot.subsystems.superstructure.wrist;
+
+public class Wrist {
+    
+}
