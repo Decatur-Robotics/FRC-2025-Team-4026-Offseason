e2014a7c96bc2dcc50483a67dc83dadd6bc54895
logan1102 <loganpark1102@gmail.com>
2025-10-28 17:48:44 -0400

    configered motors

--- FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/arm/ArmIO.java
+++ FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/arm/ArmIO.java
@@ -1,5 +1,16 @@
 package frc.robot.subsystems.superstructure.arm;
 
-public class ArmIO {
+public interface ArmIO {
+    class ArmIOInputs{
+        public ArmIOData data = new ArmIOData(false,0.0,0.0,0.0,0.0,0.0);
+    }
+    record ArmIOData(
+    boolean motorConnected,
+    double voltage,
+    double position,
+    double velocity,
+    double supplyAmps,
+    double torqueCurrent
+    ){};
     
-}
+} 
