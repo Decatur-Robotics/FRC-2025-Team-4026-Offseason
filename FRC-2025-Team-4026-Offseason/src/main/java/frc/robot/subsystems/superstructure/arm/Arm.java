954ebe494a81b2a29f3465e9e319b2c60a55e270
logan1102 <loganpark1102@gmail.com>
2025-10-30 17:48:57 -0400

    finished io and iomotor

--- FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/arm/Arm.java
+++ FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/arm/Arm.java
@@ -1,5 +1,13 @@
 package frc.robot.subsystems.superstructure.arm;
 
-public class Arm {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import org.littletonrobotics.junction.AutoLogOutput;
+
+public class Arm extends SubsystemBase{
+    private double position;
+    private double voltage;
+    private double velocity;
+
+    private boolean isEstopped;
+    private ArmIOInputsAutoLogged inputs  = new ArmIOInputsAutoLogged();
 }
--- FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/arm/ArmIO.java
+++ FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/arm/ArmIO.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.superstructure.arm;
 
-public class ArmIO {
+import org.littletonrobotics.junction.AutoLog;
+
+public interface ArmIO {
+    @AutoLog
+    class ArmIOInputs{
+        public ArmIOData data = new ArmIOData(false,0.0,0.0,0.0,0.0,0.0);
+    }
+    record ArmIOData(
+    boolean motorConnected,
+    double voltage,
+    double position,
+    double velocity,
+    double supplyAmps,
+    double torqueCurrent
+    ){}; 
     
-}
+} 
