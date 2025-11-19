eb2ad94641630374ca138979aeb4b18bf40f63dc
Charles Kennedy <82charkenn@csdecatur.net>
2025-11-19 17:39:55 -0500

    added constants for superstructure states

--- FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
+++ FRC-2025-Team-4026-Offseason/src/main/java/frc/robot/subsystems/superstructure/Superstructure.java
@@ -1,5 +1,19 @@
 package frc.robot.subsystems.superstructure;
 
+import frc.robot.subsystems.superstructure.arm.Arm;
+import frc.robot.subsystems.superstructure.elevator.Elevator;
+import frc.robot.subsystems.superstructure.intake.Intake;
+import frc.robot.subsystems.superstructure.wrist.Wrist;
+
 public class Superstructure {
-    
+    private Elevator elevator;
+    private Arm arm;
+    private Wrist wrist;
+    private Intake intake;
+    public Superstructure(Elevator elevator, Arm arm, Intake intake, Wrist wrist) {
+        this.elevator = elevator;
+        this.arm = arm;
+        this.wrist = wrist;
+        this.intake = intake;
+    }
 }
